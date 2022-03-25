b345456c17ba2f91c8d08acf9cf88377276c8731
AASTHA GUPTA <aasthagupta305@gmail.com>
2022-03-25 17:00:45 +0530

    changes in Cards

---
+++ src/components/Main/Cards/image.jsx
@@ -0,0 +1,35 @@
+const info = [
+    {
+        title: 'Instant Video Consultation',
+        desc: 'Connect within 60 secs',
+        img:
+            'https://www.practostatic.com/consumer-home/desktop/images/1597423628/dweb_instant_video_consulation.png',
+        bg: '#afcfed'
+    },
+    {
+        title: 'Find Experts Near You',
+        desc: 'Confirmed Appointments',
+        img: 'https://www.practostatic.com/consumer-home/desktop/images/1597423628/dweb_find_doctors.png',
+        bg: '#98cbd6'
+    },
+    {
+        title: 'Government Schemes',
+        desc: 'Schemes related to Agriculture',
+        img: 'https://www.practostatic.com/consumer-home/desktop/images/1597423628/dweb_medicines.png',
+        bg: '#ccd0db'
+    },
+    {
+        title: 'Crop Tests',
+        desc: 'Sample pickup at your home',
+        img: 'https://www.practostatic.com/consumer-home/desktop/images/1597423628/dweb_lab_tests.png',
+        bg: '#a2cae7'
+    },
+    {
+        title: 'Agro Updates',
+        desc: 'Daily Updates',
+        img: './updateee.png',
+        bg: '#d5d8fc'
+    }
+];
+
+export default info;
